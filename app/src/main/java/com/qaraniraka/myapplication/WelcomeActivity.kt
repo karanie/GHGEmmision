ba8802cfc0ba49d9d004cf432d65a0219130447d
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2023-12-09 15:26:08 +0700

    Set content to WelcomeScreen composable

--- app/src/main/java/com/qaraniraka/myapplication/WelcomeActivity.kt
+++ app/src/main/java/com/qaraniraka/myapplication/WelcomeActivity.kt
@@ -10,37 +10,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.qaraniraka.myapplication.ui.WelcomeScreen
 import com.qaraniraka.myapplication.ui.theme.GHGEmissionTheme
 
 class WelcomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GHGEmissionTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            WelcomeActivityApp()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun WelcomeActivityApp() {
     GHGEmissionTheme {
-        Greeting("Android")
+        WelcomeScreen()
     }
 }

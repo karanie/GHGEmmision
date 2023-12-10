cff70482aef80f0c88b8caa2a839bb51d7143fb9
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2023-12-10 13:03:26 +0700

    WelcomeActivity.kt: Add NavHost, routes and navigation

--- app/src/main/java/com/qaraniraka/myapplication/WelcomeActivity.kt
+++ app/src/main/java/com/qaraniraka/myapplication/WelcomeActivity.kt
@@ -10,6 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.qaraniraka.myapplication.ui.LoginScreen
+import com.qaraniraka.myapplication.ui.RegisterScreen
+import com.qaraniraka.myapplication.ui.Routes
 import com.qaraniraka.myapplication.ui.WelcomeScreen
 import com.qaraniraka.myapplication.ui.theme.GHGEmissionTheme
 
@@ -25,8 +33,23 @@ class WelcomeActivity : ComponentActivity() {
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun WelcomeActivityApp() {
+fun WelcomeActivityApp(navController: NavHostController = rememberNavController()) {
     GHGEmissionTheme {
-        WelcomeScreen()
+        NavHost(
+            navController = navController, startDestination = Routes.WelcomeScreen.name
+        ) {
+            composable(route = Routes.WelcomeScreen.name) {
+                WelcomeScreen(
+                    onRegisterButtonClicked = { navController.navigate(Routes.RegisterScreen.name) },
+                    onLoginButtonClicked = { navController.navigate(Routes.LoginScreen.name) }
+                )
+            }
+            composable(route = Routes.LoginScreen.name) {
+                LoginScreen()
+            }
+            composable(route = Routes.RegisterScreen.name) {
+                RegisterScreen()
+            }
+        }
     }
 }

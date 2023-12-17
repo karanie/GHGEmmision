89df7bec5a77f20370d143384ca4aecbddeacd12
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2023-12-17 16:58:03 +0700

    BerkendaraScreen.kt: Optimize imports

--- app/src/main/java/com/qaraniraka/myapplication/ui/BerkendaraScreen.kt
+++ app/src/main/java/com/qaraniraka/myapplication/ui/BerkendaraScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue

afc499d3a98d46cf739fa07dbc265bd720eedfcc
Karanie <qarani21ti@mahasiswa.pcr.ac.id>
2023-12-24 16:23:39 +0700

    GHGEmApplication.kt: Initialize userSessionPreferenceRepository

--- app/src/main/java/com/qaraniraka/myapplication/GHGEmApplication.kt
+++ app/src/main/java/com/qaraniraka/myapplication/GHGEmApplication.kt
@@ -1,26 +1,37 @@
 package com.qaraniraka.myapplication
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.qaraniraka.myapplication.data.AppContainer
 import com.qaraniraka.myapplication.data.AppDataContainer
-import kotlin.reflect.typeOf
+import com.qaraniraka.myapplication.data.UserSessionPreferencesRepository
 
 class GHGEmApplication: Application() {
     /**
      * AppContainer instance used by the rest of classes to obtain dependencies
      */
     lateinit var container: AppContainer
+    lateinit var userSessionPreferencesRepository: UserSessionPreferencesRepository
 
     override fun onCreate() {
         super.onCreate()
         Log.w("GHGEmApplication", "onCreate")
         container = AppDataContainer(this)
+        userSessionPreferencesRepository = UserSessionPreferencesRepository(dataStore)
 
         Log.w("GHGEmApplication", container.toString())
     }
 }
 fun CreationExtras.ghgEmApplication(): GHGEmApplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as GHGEmApplication)
+
+private const val USER_SESSION_PREFERENCE_NAME = "user_session_preference"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = USER_SESSION_PREFERENCE_NAME
+)
